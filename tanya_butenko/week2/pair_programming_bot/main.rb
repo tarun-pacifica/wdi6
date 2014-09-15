ef22b4081c69bfcfe5a07b38030c7ea2e156358d
ButenkoT <tanya@butenko.me>
2014-09-16 00:34:12 +1000

    more interactivity in pair_programming_bot and buttons

--- tanya_butenko/week2/pair_programming_bot/main.rb
+++ tanya_butenko/week2/pair_programming_bot/main.rb
@@ -2,45 +2,46 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 sitemap = {
-	:test => {
+	"test" => {
 		:title => "Do you have a test for that?",
 		:yes => "pass",
 		:no => "write_test"
-	}
-	:pass => {
+	},
+	"pass" => {
 		:title => "Does the test pass?",
 		:yes => "refactor",
 		:no => "write_code"
-	}
-	:write_test => {
+	},
+	"write_test" => {
 		:title => "Write a test",
 		:yes => "pass",
 		:no => nil
-	}
-	:refactor => {
+	},
+	"refactor" => {
 		:title => "Need to refactor",
 		:yes => "refactored",
 		:no => "select"
-	}
-	:refactored => {
+	},
+	"refactored" => {
 		:title => "Refactor the code",
 		:yes => "pass",
 		:no => nil
-	}
-	:write_code => {
+	},
+	"write_code" => {
 		:title => "Wright just enough code for the test to pass",
 		:yes => "pass",
 		:no => nil
-	}
-	:select => {
+	},
+	"select" => {
 		:title => "Select a new feature to implement",
 		:yes => "test",
 		:no => nil
 	}
 }
 
-get '/ppb' do
-
+get '/' do
+	id = params[:id] || 'test'
+	@item = sitemap[id] || sitemap[:test] 
 	erb :pair_prog
 end
 
