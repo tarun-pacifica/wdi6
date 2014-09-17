e3486a51f2b142b9cd1feed8c52c05d56594a6c8
Jon Bartlett <mail@jaybe.net mailto:mail@jaybe.net>
2014-09-17 14:06:40 +1000

    now uses hashes not arrays

--- jon_bartlett/week02-day01-pairprogramming/main.rb
+++ jon_bartlett/week02-day01-pairprogramming/main.rb
@@ -4,17 +4,44 @@ require 'sinatra/reloader'
 
 get '*' do
 
-	@qmap = {}
-
-  @qmap['/'] = ['Do you have a test for that?','/pass','/write_test',nil]
-  @qmap['/pass'] = ['Does the test pass?','/refactor','/write_code',nil]   
-  @qmap['/write_test'] = ['Write a test',nil,nil,'/pass']   
-  @qmap['/write_code'] = ['Just enough code for the test to pass',nil,nil,'/pass']   
-  @qmap['/refactor'] = ['Need to refactor?','/no_refactor','/new_feature',nil]   
-  @qmap['/no_refactor'] = ['Refactor code',nil,nil,'/pass']   
-  @qmap['/new_feature'] = ['Select a new feature to implement',nil,nil,'/']     
+  @qmap = {
+    '/' => {
+      :question => 'Do you have a test for that?',
+      :yes_path => '/pass',
+      :no_path => '/write_test',
+      :done_path => nil},
+    '/pass' => {
+      :question => 'Does the test pass?',
+      :yes_path => '/refactor',
+      :no_path => '/write_code',
+      :done_path => nil},
+    '/write_test' => {
+      :question => 'Write a test',
+      :yes_path => nil,
+      :no_path => nil,
+      :done_path => '/pass'},
+    '/write_code' => {
+      :question => 'Just enough code for the test to pass',
+      :yes_path => nil,
+      :no_path => nil,
+      :done_path => '/pass'},
+    '/refactor' => {
+      :question => 'Need to refactor?',
+      :yes_path => '/no_refactor',
+      :no_path => '/new_feature',
+      :done_path => nil},
+    '/no_refactor' => {
+      :question => 'Refactor code',
+      :yes_path => nil,
+      :no_path => nil,
+      :done_path => '/pass'},
+    '/new_feature' => {
+      :question => 'Select a new feature to implement',
+      :yes_path => nil,
+      :no_path => nil,
+      :done_path => '/'}
+} 
 
   erb :pp  
-
 end
 
