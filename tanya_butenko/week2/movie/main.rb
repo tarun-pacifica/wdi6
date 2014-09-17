ba3e038b338ccd8ff8f105063919fe00b5c9407b
ButenkoT <tanya@butenko.me>
2014-09-18 01:33:02 +1000

    some css on movie db

--- tanya_butenko/week2/movie/main.rb
+++ tanya_butenko/week2/movie/main.rb
@@ -1,16 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
+require 'uri'
 
 get '/' do
-	erb :movies	
-end
-
-get '/search_result' do
-	title = params['search_string'].sub(' ', '+')
+	unless params[:movie_poster].nil?
+	title = URI.escape(params['movie_poster'])
 	movie_results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ title }") # Fetch the movie data
 	@movie = JSON.parse(movie_results)['Poster'] # Convert the raw 
+	end
 
-	erb :search
+	erb :movies
 	
 end
