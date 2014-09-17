4ab29dc1d8615c96477e27c5ce1de0e6f53f1673
jadefeng <j.feng.1991@gmail.com>
2014-09-17 20:35:28 +1000

    Updated things but not sure what. Hmmm

--- jade_feng/W2D8/Movies/main.rb
+++ jade_feng/W2D8/Movies/main.rb
@@ -15,11 +15,29 @@ end
 
 get "/search_results" do 
 	# if params["movie_name"]
-		@title_result = params["movie_name"].downcase.delete(' ').strip
+		@title_result = params["movie_name"].downcase.strip.gsub! /\s+/, '+'
 		movie_result = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ @title_result }") 
 		@movie = JSON.parse movie_result
+	# Output here
+		@title = @movie['Title'].to_s
 		@poster = @movie['Poster'].to_s
-
-	# end
+		@year = @movie['Year'].to_s
+		@rated = @movie['Rated'].to_s
+		@released = @movie['Released'].to_s
+		@runtime = @movie['Runtime'].to_s
+		@genre = @movie['Genre'].to_s
+		@director = @movie['Director'].to_s
+		@writer = @movie['Writer'].to_s
+		@actors = @movie['Actors'].to_s
+		@plot = @movie['Plot'].to_s
+		@language = @movie['Language'].to_s
+		@country = @movie['Country'].to_s 
+		@awards = @movie['Awards'].to_s
+		# @metascore = @movie['Metascore'].to_s
+		@imdbRating = @movie['imdbRating'].to_s
+		# @imdbVotes ] = @movie['imdbVotes'].to_s
+		# @imdbID = @movie['imdbID'].to_s
+		# @type = @movie['Type'].to_s
+		# @response = @movie['Response'].to_s
 	erb :search_results
 end
