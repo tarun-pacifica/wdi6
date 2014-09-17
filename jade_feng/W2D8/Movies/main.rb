88b71b51cf67989c352c9ceaee9d30ef710dfa2f
jadefeng <j.feng.1991@gmail.com>
2014-09-18 00:38:25 +1000

    Love the cats

--- jade_feng/W2D8/Movies/main.rb
+++ jade_feng/W2D8/Movies/main.rb
@@ -14,18 +14,22 @@ get "/" do
 end
 
 get "/search_results" do 
-	# if params["movie_name"]
 		@title_result = params["movie_name"].downcase.strip.gsub! /\s+/, '+'
 		movie_result = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ @title_result }") 
 		@movie = JSON.parse movie_result
 	# Output here
 		@title = @movie['Title'].to_s
 		@poster = @movie['Poster'].to_s
+		if @poster != "N/A"
+			@poster = @movie['Poster'].to_s
+		else
+			@poster = "http://www.notsuperhuman.com/wp-content/uploads/2011/03/5501618311_1eeec26185_z_large.jpg"
+		end
 		@year = @movie['Year'].to_s
 		@rated = @movie['Rated'].to_s
 		@released = @movie['Released'].to_s
 		@runtime = @movie['Runtime'].to_s
-		@genre = @movie['Genre'].to_s
+		@genre = @movie['Genre']
 		@director = @movie['Director'].to_s
 		@writer = @movie['Writer'].to_s
 		@actors = @movie['Actors'].to_s
@@ -36,8 +40,16 @@ get "/search_results" do
 		# @metascore = @movie['Metascore'].to_s
 		@imdbRating = @movie['imdbRating'].to_s
 		# @imdbVotes ] = @movie['imdbVotes'].to_s
-		# @imdbID = @movie['imdbID'].to_s
-		# @type = @movie['Type'].to_s
-		# @response = @movie['Response'].to_s
-	erb :search_results
+		@imdbID = @movie['imdbID'].to_s
+		@type = @movie['Type'].to_s
+		# @response = @movie['Response']
+	if @title.empty? 
+		erb :not_found
+	else
+		erb :search_results
+	end 
+end
+
+get "/not_found" do 
+	erb :not_found
 end
