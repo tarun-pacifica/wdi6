75f9f9d0944ba9d7d4f223b1f5fc4812663ffe1e
Nick Tho <matenaar92@gmail.com>
2014-09-18 09:24:01 +1000

    Add Search Wall

--- nick_matenaar/w2d03/postr/main.rb
+++ nick_matenaar/w2d03/postr/main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'HTTParty'
 
+$posters_hash = {} # Hold URLS to poster imgs with search tags
+
 get '/' do 
 	erb :home
 end
@@ -16,14 +18,23 @@ get '/search' do
 	# Check if real 
 	unless movie['Error']
 		@poster_url = movie['Poster']
+		$posters_hash[@movie_name] = @poster_url
 		rating = (movie['imdbRating'].to_f / 2).to_i
 		@rating = "" 
 		rating.times do 
 			@rating << "&star;"
 		end 
-
+		
 	else
 		@error=true
 	end
 	erb :search
 end 
+
+get "/searchwall" do 
+	@code = " "
+	$posters_hash.each do |name, img|
+		@code << '<a href="/search?search_string='+name+'"><img src="'+img+'" class="resize"></a>'
+	end 
+	erb :searchwall
+end
