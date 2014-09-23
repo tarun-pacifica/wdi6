0310e84722c83206b3b8cd7dd1f7049e2c8670d0
lawrencely <lawrence.ly667@gmail.com>
2014-09-23 21:21:37 +1000

    hate this shit

--- lawrence_ly/movie-review/main.rb
+++ lawrence_ly/movie-review/main.rb
@@ -1,15 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
+require 'pry'
 
 get	'/' do
+
+	  if params['movie_title']
+	      @title = params['movie_title'].gsub(' ','+')
+	      movie_result = HTTParty.get("http://www.omdbapi.com/?i=&s=#{ @title }")
+	      @movies = JSON.parse movie_result
+	  end
 	erb :home
 end
 
 get '/results' do
+	binding.pry
+	if params['movie_title']
+	      @title = params['movie_title'].gsub(' ','+')
+	      movie_result = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ @title }")
+	      @movies = JSON.parse movie_result
+	  end
 	erb :results
 end
 
+get '/results/:id' do
+
+end
 
 
 
