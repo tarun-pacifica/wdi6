8a8ed5c488e480e032f5ec0a9a5409273dc5532d
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-03 20:54:20 +1000

    WOW. Totally left out Postgres. Got that working and bundled again. Good to go

--- james_leboeuf/project_1_worthia/worthia_app/db/schema.rb
+++ james_leboeuf/project_1_worthia/worthia_app/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141003053117) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "item_prices", force: true do |t|
     t.integer  "item_id"
     t.integer  "user_id"
