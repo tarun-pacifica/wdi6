56fe831c66d3d53ba795a836fe0dfb604124f43f
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-03 20:34:42 +1000

    Successful seed and beginning home route implementation.

--- james_leboeuf/project_1_worthia/worthia_app/db/seeds.rb
+++ james_leboeuf/project_1_worthia/worthia_app/db/seeds.rb
@@ -1,1 +1,9 @@
+User.destroy_all
+Item.destroy_all
+
+u1 = User.create(:name => 'James')
+u2 = User.create(:name => 'Lawrence')
+
+i1 = Item.create(:name => 'Apple')
+i2 = Item.create(:name => 'Hammock')
 
