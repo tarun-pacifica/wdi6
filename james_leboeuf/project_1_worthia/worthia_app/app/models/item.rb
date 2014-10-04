70dd4c5d84368bd3d49103353490e5ff9af897fc
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-04 18:40:01 +1000

    Joel. Your a wizard. Thanks for that simple fix

--- james_leboeuf/project_1_worthia/worthia_app/app/models/item.rb
+++ james_leboeuf/project_1_worthia/worthia_app/app/models/item.rb
@@ -19,13 +19,13 @@ class Item < ActiveRecord::Base
   pg_search_scope :search_by_tsearch_but_rank_by_trigram, against: [:name, :content], using: {tsearch: {dictionary: "english"}}, ignoring: :accents
 
   # :search, against: [:name, :content]
-
+  # where(nil) => Displays all results from the database
 
   def self.text_search(query)
     if query.present?
       search_by_tsearch_but_rank_by_trigram(query)
     else
-      where(nil)
+      []
     end
   end
 end
