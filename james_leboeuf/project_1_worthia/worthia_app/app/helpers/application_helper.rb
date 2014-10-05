a13fabe079c3eb94cfe466d04e370421fa3226ce
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-05 16:47:09 +1100

    Added My Profile tab

--- james_leboeuf/project_1_worthia/worthia_app/app/helpers/application_helper.rb
+++ james_leboeuf/project_1_worthia/worthia_app/app/helpers/application_helper.rb
@@ -7,6 +7,7 @@ module ApplicationHelper
       nav += "<li>" + link_to('Edit profile', edit_user_path) + "</li> "
       nav += "<li>" + link_to('Add item', new_item_path) + "</li> "
       nav += "<li>#{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => 'Are you sure?'}) }</li> "
+      nav += "<li>" + link_to('My Profile', user_path(@current_user)) + "<li> "
     else
       nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
       nav += "<li>#{ link_to('Sign in', login_path) }</li>"
