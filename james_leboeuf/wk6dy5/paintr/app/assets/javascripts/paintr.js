a0cbb350dbd6fae941b6fe1fcd05332aed61e0b3
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-19 21:11:16 +1100

    Fixed the mouse movement(s) for paintr.

--- james_leboeuf/wk6dy5/paintr/app/assets/javascripts/paintr.js
+++ james_leboeuf/wk6dy5/paintr/app/assets/javascripts/paintr.js
@@ -1,7 +1,6 @@
 var $canvas = $("#canvas");
 var mouseDown = false;
 
-
 var addColor = function () {
   var color = $('#color').val();
   var $box = $('<div/>');
@@ -16,27 +15,25 @@ var setColor = function () {
 };
 
 
-
 var paintPixel = function () {
-  var color = $('#current_color').css('background-color');
-  $(this).css('background-color', color);
+  if (mouseDown) {
+    console.log(this);
+    var color = $('#current_color').css('background-color');
+    $(this).css('background-color', color);
+  };
 };
 
 $(document).ready(function () {
   $('#add_color').on('click', addColor);
   $('#colors').on('click', '.box', setColor); // Delegation!
-  // $('#canvas').on('mousemove', '.pixel', paintPixel);
-  $("#canvas").mousedown(function(){
-  mouseDown = true;
-    }).mousemove(function(){
-      if (mouseDown) {
-        $("#canvas").append('.pixel', paintPixel);
-      }
+  $('#canvas').mousedown(function(){
+    mouseDown = true;
     }).mouseup(function(){
       mouseDown = false;
     }).mouseleave(function(){
       $("#canvas").mouseup();
-    });
+  });
+  $('#canvas').on('mouseover', '.pixel', paintPixel);
   var pixels = '';
   for(var i = 0; i < 10000; i++) {
     pixels += '<div class="pixel"></div>';
